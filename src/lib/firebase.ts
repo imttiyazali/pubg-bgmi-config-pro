93c9a51a44365a29d2c98a484a7b5efa5d710306
imttiyaz Ali <gorsiimtiyax99066@gmail.com>
2025-07-23 14:34:47 +0000

    login signup abhi nhi ho rha hai

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,19 +1,18 @@
-
 // Import the functions you need from the SDKs you need
-import { initializeApp, getApps, getApp, type FirebaseOptions } from "firebase/app";
+import { initializeApp, getApps, getApp, type FirebaseApp, type FirebaseOptions } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig: FirebaseOptions = {
-  apiKey: "AIzaSyAzOJV20yi-zBOD57WuTNTE4P003OG0Z8c",
-  authDomain: "bgmi-config-pro.firebaseapp.com",
-  projectId: "bgmi-config-pro",
-  storageBucket: "bgmi-config-pro.firebasestorage.app",
-  messagingSenderId: "318946056014",
-  appId: "1:318946056014:web:c33e9d6c8123104bdd301f",
+    apiKey: "AIzaSyAzOJV20yi-zBOD57WuTNTE4P003OG0Z8c",
+    authDomain: "bgmi-config-pro.firebaseapp.com",
+    projectId: "bgmi-config-pro",
+    storageBucket: "bgmi-config-pro.firebasestorage.app",
+    messagingSenderId: "318946056014",
+    appId: "1:318946056014:web:c33e9d6c8123104bdd301f",
 };
 
 // Initialize Firebase
-const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+const app: FirebaseApp = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 const auth = getAuth(app);
 
 export { app, auth };
