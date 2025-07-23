7dce34c24512734e6d60946ebd13a40565c8bb2b
imttiyaz Ali <gorsiimtiyax99066@gmail.com>
2025-07-23 16:57:41 +0000

    Mai generate config pe click kiya to ye error aarha hai... an error occu

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,18 +1,19 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp, getApps, getApp, type FirebaseApp, type FirebaseOptions } from "firebase/app";
+import { initializeApp, getApps, getApp, type FirebaseApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
-const firebaseConfig: FirebaseOptions = {
+const firebaseConfig = {
     apiKey: "AIzaSyAzOJV20yi-zBOD57WuTNTE4P003OG0Z8c",
     authDomain: "bgmi-config-pro.firebaseapp.com",
     projectId: "bgmi-config-pro",
     storageBucket: "bgmi-config-pro.firebasestorage.app",
     messagingSenderId: "318946056014",
-    appId: "1:318946056014:web:c33e9d6c8123104bdd301f",
+    appId: "1:318946056014:web:c33e9d6c8123104bdd301f"
 };
+
 
 // Initialize Firebase
 const app: FirebaseApp = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 const auth = getAuth(app);
 
-export { app, auth };
+export { app, auth, initializeApp };
