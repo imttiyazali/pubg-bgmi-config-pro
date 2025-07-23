1396ad99d9f9351dbdba96882b62c191b0104ca0
imttiyaz Ali <gorsiimtiyax99066@gmail.com>
2025-07-23 12:50:30 +0000

    File changes

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,3 +1,4 @@
+
 'use server';
 // Import the functions you need from the SDKs you need
 import { initializeApp, getApps, type FirebaseOptions } from "firebase/app";
@@ -10,20 +11,8 @@ const firebaseConfig: FirebaseOptions = {
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  appId: process.env.NEXT_PULIC_FIREBASE_APP_ID,
 };
-
-const toSnakeCase = (str: string) => {
-  return str.replace(/[A-Z]/g, letter => `_${letter}`).toUpperCase();
-}
-
-// Validate environment variables
-for (const key in firebaseConfig) {
-  if (firebaseConfig[key as keyof FirebaseOptions] === undefined) {
-    const envVarName = `NEXT_PUBLIC_FIREBASE_${toSnakeCase(key)}`;
-    throw new Error(`Missing Firebase config key: ${key}. Please set the corresponding ${envVarName} environment variable in your Vercel project settings.`);
-  }
-}
 
 // Initialize Firebase
 let app;
